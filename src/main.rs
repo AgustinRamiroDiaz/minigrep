4dbe8dd609624fcf53ef4a5a09b7812bd508f99a
Agustín Díaz <agustin.ramiro.diaz@gmail.com>
2022-06-11 19:13:53 +0000

    Decouple configuration parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let arguments: Vec<String> = env::args().collect();
+
+    let config = parse_configuration(&arguments);
+    println!(
+        "Called as '{}', searching for '{}' in file '{}'",
+        config.invoked_name, config.query, config.file_name
+    );
+
+    let content =
+        fs::read_to_string(config.file_name).expect("I shouldn't panic for reading a file 🙃");
+
+    println!("Contets of '{}':\n{}", config.file_name, content);
+}
+
+struct configuration<'a> {
+    invoked_name: &'a String,
+    query: &'a String,
+    file_name: &'a String,
+}
+
+fn parse_configuration(arguments: &[String]) -> configuration {
+    configuration {
+        invoked_name: &arguments[0],
+        query: &arguments[1],
+        file_name: &arguments[2],
+    }
 }
