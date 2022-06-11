c3b73e8192a45ec69ae62ef2cf1e4d03bd46f267
Agustin Ramiro Diaz <agustin.ramiro.diaz@gmail.com>
2022-06-11 19:51:28 +0000

    Error handling

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,42 @@
-use std::{env, fs};
+use std::{env, fs, process};
 
 fn main() {
     let arguments: Vec<String> = env::args().collect();
 
-    let config = parse_configuration(&arguments);
+    let Configuration {
+        invoked_name,
+        query,
+        file_name,
+    } = Configuration::new(&arguments).unwrap_or_else(|err| {
+        println!("Error creating configuration: {}", err);
+        process::exit(1);
+    });
     println!(
         "Called as '{}', searching for '{}' in file '{}'",
-        config.invoked_name, config.query, config.file_name
+        invoked_name, query, file_name
     );
 
-    let content =
-        fs::read_to_string(config.file_name).expect("I shouldn't panic for reading a file 🙃");
+    let content = fs::read_to_string(&file_name).expect("I shouldn't panic for reading a file 🙃");
 
-    println!("Contets of '{}':\n{}", config.file_name, content);
+    println!("Contets of '{}':\n{}", file_name, content);
 }
 
-struct configuration<'a> {
-    invoked_name: &'a String,
-    query: &'a String,
-    file_name: &'a String,
+struct Configuration {
+    invoked_name: String,
+    query: String,
+    file_name: String,
 }
 
-fn parse_configuration(arguments: &[String]) -> configuration {
-    configuration {
-        invoked_name: &arguments[0],
-        query: &arguments[1],
-        file_name: &arguments[2],
+impl Configuration {
+    fn new(arguments: &[String]) -> Result<Configuration, &'static str> {
+        if arguments.len() < 3 {
+            return Err("Not enough arguments");
+        }
+
+        Ok(Configuration {
+            invoked_name: arguments[0].clone(),
+            query: arguments[1].clone(),
+            file_name: arguments[2].clone(),
+        })
     }
 }
