487ee86a87eebd1f5fab0cf56824a6c89ba9c49a
Agustin Ramiro Diaz <agustin.ramiro.diaz@gmail.com>
2022-06-14 17:27:11 +0000

    Implement search

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,9 @@ use std::{error::Error, fs};
 pub fn run(config: Configuration) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_name)?;
 
-    println!("With text:\n{}", contents);
+    search(&config.query, &contents)
+        .iter_mut()
+        .for_each(|result| println!("{result}"));
 
     Ok(())
 }
@@ -25,5 +27,29 @@ impl Configuration {
             query: arguments[1].clone(),
             file_name: arguments[2].clone(),
         })
+    }
+}
+
+fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    return content
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "l";
+        let contents = "\
+la peque
+peque
+peque
+pasteleria
+";
+        assert_eq!(vec!["la peque", "pasteleria"], search(query, contents))
     }
 }
